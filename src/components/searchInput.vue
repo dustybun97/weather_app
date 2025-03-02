c53ad5dddd773fd2256239ec89e0f3658d0acc89
podusit <d.rimdusit@gmail.com>
2025-03-02 14:13:32 +0700

    feat:project init

---
+++ src/components/searchInput.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive } from "vue";
+const searchTerm = reactive({
+  search: "",
+});
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout);
+  searchTerm.timeout = setTimeout(() => {
+    console.log(searchTerm.search);
+  }, 500);
+};
+</script>
+<template>
+  <div>
+    <!-- search box -->
+    <form>
+      <div class="flex items-center bg-white rounded-lg shadow-lg">
+        <i class="fa-solid fa-search p-2 text-indigo-600"></i>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
+          v-model="searchTerm.search"
+          @input="handleSearch"
+        />
+      </div>
+    </form>
+    <!-- search suggestions -->
+    <div class="bg-white my-2 rounded-lg shadow-lg">
+      <div>
+        <button
+          class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
+        ></button>
+      </div>
+    </div>
+  </div>
+</template>
