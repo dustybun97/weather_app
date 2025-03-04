daf61446fc579009dc4c624a3338fe2f4b106512
podusit <d.rimdusit@gmail.com>
2025-03-04 16:18:24 +0700

    feat:weather card and forcast

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,34 @@
 <script setup>
-  import searchInput from './components/searchInput.vue'
+import { ref } from "vue";
+import searchInput from "./components/searchInput.vue";
+import WeatherCard from "./components/weatherCard.vue";
+const places = ref([]);
+const addPlace = (data) => {
+  places.value.push(data);
+};
 </script>
 
 <template>
   <main>
     <!-- date -->
-    <div class=" text-center mb-6">
-      {{ new Date().toLocaleDateString('en-us',{
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }) }}
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString("en-us", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })
+      }}
     </div>
     <!-- search box -->
     <div class="">
-      <searchInput />
+      <searchInput @place-data="addPlace" />
     </div>
-    <!-- weather report -->
-    <div class="flex justify-center mt-6">
-      <div class="bg-gray-200 p-4">
-        <h2 class="text-center">City Name</h2>
-        <div class="flex justify-between">
-          <p>Temperature</p>
-          <p>25°C</p>
-        </div>
-        <div class="flex justify-between">
-          <p>Weather</p>
-          <p>Cloudy</p>
-        </div>
-        <div class="flex justify-between">
-          <p>Wind</p>
-          <p>5 km/h</p>
-        </div>
-        <div class="flex justify-between">
-          <p>Humidity</p>
-          <p>50%</p>
-        </div>
-      </div>
-    </div>
-    
+    <!-- weather card -->
+     <div v-for="place in places" :key="place.location.name">
+      <WeatherCard :place="place"/>
+     </div>
   </main>
 </template>
 
--- src/components/searchInput.vue
+++ src/components/searchInput.vue
@@ -1,14 +1,38 @@
 <script setup>
 import { reactive } from "vue";
+
+const emit = defineEmits(["place-data"]);
+
 const searchTerm = reactive({
   search: "",
+  timeout: null,
+  results: null,
 });
 
 const handleSearch = () => {
   clearTimeout(searchTerm.timeout);
-  searchTerm.timeout = setTimeout(() => {
-    console.log(searchTerm.search);
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.search !== "") {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=4916bbe38db34274af241906250403&q=${searchTerm.search}`
+      );
+      const data = await res.json();
+      console.log(data);
+      searchTerm.results = data;
+    } else {
+      searchTerm.results = null;
+    }
   }, 500);
+};
+
+const getWeather = async (id) => {
+  const res =
+    await fetch(`http://api.weatherapi.com/v1/forecast.json?key=4916bbe38db34274af241906250403&q=id:${id}&days=3&aqi=no&alerts=no
+`);
+  const data = await res.json();
+  emit("place-data", data);
+  searchTerm.search = "";
+  searchTerm.results = null;
 };
 </script>
 <template>
@@ -28,10 +52,15 @@ const handleSearch = () => {
     </form>
     <!-- search suggestions -->
     <div class="bg-white my-2 rounded-lg shadow-lg">
-      <div>
-        <button
-          class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
-        ></button>
+      <div v-if="searchTerm.results !== null">
+        <div v-for="place in searchTerm.results" :key="place.id">
+          <button
+            @click="getWeather(place.id)"
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
+          >
+            {{ place.name }}, {{ place.region }}, {{ place.country }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
